6321d7ba302d88839b6d03c43bc2ef9b25fb2695
Brian Douglas <bdougie@users.noreply.github.com>
2019-09-23 14:12:27 -0700

    Update run.rb

--- run.rb
+++ run.rb
@@ -1,15 +1,13 @@
 require 'octokit'
 
-NWO = ENV["GITHUB_REPOSITORY"]
-ISSUE_TEMPLATE = File.read(File.join(File.dirname(__FILE__), "template.md"))
+repo = ENV["GITHUB_REPOSITORY"]
+label = ENV["LABEL"]
 
 client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
 client.auto_paginate = true
 
-open_issues = client.list_issues(NWO, { :labels => "weekly-meeting" })
+open_issues = client.list_issues(repo, { :labels =>  label})
 
 open_issues.each do |issue|
-  client.close_issue(NWO, issue.number)
+  client.close_issue(repo, issue.number)
 end
-
-client.create_issue(NWO, "Weekly Meeting" , ISSUE_TEMPLATE, { :labels => "weekly-meeting" })
