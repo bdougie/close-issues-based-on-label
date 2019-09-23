76a168adc0e01505c083183712e0faf2e6d949ec
Brian Douglas <bdougie@users.noreply.github.com>
2019-09-23 14:14:04 -0700

    switch to ruby (#1)
    * switch to ruby

---
+++ run.rb
@@ -0,0 +1,13 @@
+require 'octokit'
+
+repo = ENV["GITHUB_REPOSITORY"]
+label = ENV["LABEL"]
+
+client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
+client.auto_paginate = true
+
+open_issues = client.list_issues(repo, { :labels =>  label})
+
+open_issues.each do |issue|
+  client.close_issue(repo, issue.number)
+end
