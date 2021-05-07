1db9d8665ecb4afb0b11f99ee20a4d7e38dffa04
Anthony Sterling <anthonysterling@github.com>
2021-05-07 14:59:43 +0100

    Update action.rb (#2)

--- action.rb
+++ action.rb
@@ -6,9 +6,8 @@ label = ENV["LABEL"]
 client = Octokit::Client.new(:access_token => ENV["GITHUB_TOKEN"])
 client.auto_paginate = true
 
-open_issues = client.list_issues(repo, { :labels =>  label})
+open_issues = client.list_issues(repo, { :labels =>  label, :state => 'open'})
 
 open_issues.each do |issue|
   client.close_issue(repo, issue.number)
-  client.remove_label(repo, issue.number, label)
 end
